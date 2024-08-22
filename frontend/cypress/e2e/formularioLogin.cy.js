11ddcf22860a9b04304c52d506e81b1abe5f6148
Emanuellys2 <duarte.emanuellys2@gmail.com>
2024-08-22 11:38:47 -0300

    Teste de validação do email

--- frontend/cypress/e2e/formularioLogin.cy.js
+++ frontend/cypress/e2e/formularioLogin.cy.js
@@ -7,7 +7,15 @@ describe('Formulário de login', () => { // contexto do nosso teste
         cy.getByData('email-input').type('neilton@alura')
         cy.getByData('senha-input').type('123456')
         cy.getByData('botao-enviar').click()
-        cy.getByData('mensagem-erro').should('exist').and('have-text','o email digitado é inválido')
+        cy.getByData('mensagem-erro').should('exist').and('have.text','O email digitado é inválido')
+})
+    it.only('Não deve permitir um campo em branco', () => {
+        cy.getByData('botao-login').click()
+        // cy.getByData('email-input').type('') /* não passaremos nenhuma informação no campo email, comentando esta linha, pois o comando type não aceita parâmetro vazio */
+        cy.getByData('senha-input').type('123456')
+        cy.getByData('botao-enviar').click()
+        cy.getByData('mensagem-erro').should('exist').and('have.text', 'O campo email é obrigatório') /* a
+    mensagem exibida deve ser: "o campo email é obrigatório" */
     })
 })
     
