dc5c0c8208c8b775b690c9c9f44dc2115bdb269b
Emanuellys2 <duarte.emanuellys2@gmail.com>
2024-08-23 15:08:42 -0300

    Test, exploração de outras possibilidades de transação

--- frontend/cypress/e2e/paginas.cy.js
+++ frontend/cypress/e2e/paginas.cy.js
@@ -6,11 +6,14 @@ describe('Testando múltiplas páginas', () => {
     cy.getByData('senha-input').type('123456')
     cy.getByData('botao-enviar').click()
     cy.location('pathname').should('eq','/home')
+    })
+
+    it('Deve verificar a página de cartões', () => {
     cy.getByData('app-home').find('a').eq(1).click()
     cy.getByData('titulo-cartoes').should('exist').and('have.text', 'Meus cartões')
     cy.location('pathname').should('eq','/home/cartoes')
     cy.getByData('app-home').find('a').eq(2).click()
-    cy.location('pathname').should('eq','/home/servicos')
+    cy.location('titulo-servico').should('eq','/home/servicos')
     })
 })
     
