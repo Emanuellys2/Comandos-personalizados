6725819f48c796c4ace3f7cf40e4183cf2a9f34b
Emanuellys2 <duarte.emanuellys2@gmail.com>
2024-08-29 11:23:49 -0300

    Teste de implementação de dispositivos móveis

---
+++ frontend/cypress/e2e/dispositivosMoveis.cy.js
@@ -0,0 +1,45 @@
+describe ('Testando dispositivos móveis', () => {
+    beforeEach(() => {
+        cy.viewport('iphone-6')
+    })
+    it ('Deve existir um menu burguer', () => {
+        cy.visit('/')
+
+        cy.getByData('botao-login').click()
+        cy.getByData('email-input').type('neilton@alura.com')
+
+        cy.getByData('senha-input').type('123456')
+        cy.getByData('botao-enviar').click()
+
+        cy.location('pathname').should('eq','/home')
+
+        cy.getByData('menu-burguer').click()
+        cy.getByData('menu-lateral').find('a').eq(3).click()
+
+        cy.location('pathname').should('eq','/home/investimentos')
+
+    })
+})
+
+context('Resolução do iphone-5 ', () => {
+    beforeEach(() => {
+      /* roda os testes como se fossem em um dispositivo com a resolução de um iphone-5 */
+      cy.viewport('iphone-5')
+    })
+    it ('Deve existir um menu burguer', () => {
+        cy.visit('/')
+
+        cy.getByData('botao-login').click()
+        cy.getByData('email-input').type('neilton@alura.com')
+
+        cy.getByData('senha-input').type('123456')
+        cy.getByData('botao-enviar').click()
+
+        cy.location('pathname').should('eq','/home')
+
+        cy.getByData('menu-burguer').click()
+        cy.getByData('menu-lateral').find('a').eq(3).click()
+
+        cy.location('pathname').should('eq','/home/investimentos')
+  })
+})
